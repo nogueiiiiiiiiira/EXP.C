269a0e79bf2773ecb7a8e7aae58f5afce3362c3b
Karen <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2025-08-25 09:22:20 -0300

    Update mqtt.py

--- P1/mqtt.py
+++ P1/mqtt.py
@@ -6,7 +6,7 @@ from hcsr04 import HCSR04
 
 led = Pin(2, Pin.OUT)
 buzzer = PWM(Pin(15))
-buzzer.duty_u16(0)
+buzzer.duty(0)
 sensor = HCSR04(trigger_pin=5, echo_pin=18, echo_timeout_us=10000)
 parar = False
 
@@ -15,11 +15,11 @@ distancia_antiga = sensor.distance_cm()
 # Configuração MQTT 
 MQTT_CLIENT_ID = "ClienteID"
 MQTT_BROKER = "broker.mqttdashboard.com"
-MQTT_TOPIC_SEND = "exp.criativa/espparapc"  
-MQTT_TOPIC_RECEIVE = "exp.criativa/pcparaesp" 
+MQTT_TOPIC_SEND = "exp.criativa/espparapc2025"  
+MQTT_TOPIC_RECEIVE = "exp.criativa/pcparaesp2025" 
 
 # Configuração da rede Wi-Fi
-WIFI_SSID = "Wokwi-GUEST"
+WIFI_SSID = "Visitantes"
 WIFI_PASSWORD = ""
 
 # Função de callback para recebimento de mensagens MQTT
@@ -49,29 +49,45 @@ client.subscribe(MQTT_TOPIC_RECEIVE)
 print("Conectado ao broker MQTT...")
 
 while not parar:
-    client.check_msg()
-    sleep(0.1)
-
+    
+    distancia_antiga = sensor.distance_cm()
+    print(f"\nDistância atual: {distancia_antiga} cm")
+    sleep(2)
     distancia_nova = sensor.distance_cm()
+    
+    sleep(2)
+    
+    diferenca = distancia_nova - distancia_antiga
 
-    print(f"Distância atual: {distancia_nova} cm")
-    if distancia_nova - distancia_antiga > 20:
-        print("Alteração de posição detectada!")
+    if diferenca > 20:
         client.publish(MQTT_TOPIC_SEND, "Alteracao de posicao detectada")
+        print("\nAlteração de posição detectada! Diferença de cm foi de:", diferenca, "cm")
+        sleep(5)
+        client.check_msg()
+        parar = True
+        
+    else:
+        print("\nA nova posicao foi menor que a antiga posicao")
 
-    distancia_antiga = distancia_nova
+print("\nParando leituras. Ligando LED e buzzer...")
 
-print("Parando leituras. Ligando LED e buzzer...")
-for i in range(3):
-    led.value(1)
-    sleep(0.5)
-    led.value(0)
-    sleep(0.5)
+led.value(1)
+sleep(0.5)
+led.value(0)
+sleep(0.5)
+led.value(1)
+sleep(0.5)
+led.value(0)
+sleep(0.5)
+led.value(1)
+sleep(0.5)
+led.value(0)
+sleep(0.5)
 
-buzzer.freq(1000)
-buzzer.duty_u16(32768)
+buzzer.freq(500)
+buzzer.duty(500)
 sleep(2)
-buzzer.duty_u16(0)
+buzzer.duty(0)
 
 client.disconnect()
-print("Código encerrado")
+print("\nCódigo encerrado")
